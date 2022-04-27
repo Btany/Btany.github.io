4f16a763e93050bed851e64d94f99fab57474b31
Bárbara Tany Barreto Martins Pessoa <b.martins.pessoa@avanade.com>
2022-04-27 15:11:19 -0300

    Aula 3

---
+++ assets/js/todo.js
@@ -0,0 +1,29 @@
+console.log("Vai Corinthias!");
+console.info("Vai Corinthias!");
+console.error("Forçando um erro!");
+console.warn("Forçando um alerto!");
+
+//const- constante - não muda nunca
+const nome = 'Bárbara Tany';
+//let - variável - pode mudar a qualquer momento
+let email = 'barbaratany@gmail.com';
+
+
+
+
+console.log(`Nome: ${nome} Email: ${email}`);
+
+if (nome == "Bárbara Tany") {
+    console.log("é igual");
+}
+
+function escreve (x) {
+    console.log("Frase: " + x);
+}
+
+escreve(`Seja bem-vindo ${nome}`);
+escreve("Treinamento HTML5, CSS3 E JS");
+
+
+
+
